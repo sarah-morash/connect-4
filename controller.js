1c3da5a25e2cc8b4f98cb54d264d48b2bb19ba19
Morash, Sarah <Sarah.Morash@T4G.com>
2016-11-13 16:39:36 -0400

    First win condition

--- controller.js
+++ controller.js
@@ -5,9 +5,9 @@ var app = angular.module("Connect4", []);
         var gameBoard = {};
         gameBoard.rows = [];
 
-        for(var row = 0; row < 6; row++){
+        for(var row = 0; row < 3; row++){
           var currRow = [];
-          for (var col = 0; col < 7; col++){
+          for (var col = 0; col < 3; col++){
             var gamePiece = {
               selected: false,
               piece: "_"
@@ -19,12 +19,32 @@ var app = angular.module("Connect4", []);
         return gameBoard;
       }
 
+      function checkRowsForWinner(board){
+        for(var i = 0; i < board.rows.length; i++){
+          var winner = true;
+          for(var j = 0; j < board.rows[i].length; j++){
+            if (board.rows[i][j].piece === "_"){
+              winner=false;
+            }
+          }
+          if (winner){
+            return true;
+          }
+        }
+
+        return false;
+      }
+
       $scope.addPiece = function(piece) {
 
         if (!piece.selected)
         {
           piece.selected=true;
           piece.piece = "X";
+        }
+
+        if (checkRowsForWinner($scope.gameBoard)){
+          alert("YOU ARE AWESOME! YOU WON!");
         }
       }
 
