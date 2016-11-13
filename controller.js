66aab84efba8a643ed32ef0cbcb16f633b1b09b5
Morash, Sarah <Sarah.Morash@T4G.com>
2016-11-13 16:01:43 -0400

    Created add piece function

--- controller.js
+++ controller.js
@@ -2,7 +2,9 @@ var app = angular.module("Connect4", []);
     app.controller("Connect4Controller", ["$scope", function($scope) {
 
       function createGameBoard() {
-        var gameBoard = [];
+        var gameBoard = {};
+        gameBoard.rows = [];
+
         for(var row = 0; row < 6; row++){
           var currRow = [];
           for (var col = 0; col < 7; col++){
@@ -12,9 +14,13 @@ var app = angular.module("Connect4", []);
             };
             currRow.push(gamePiece);
           }
-          gameBoard.push(currRow);
+          gameBoard.rows.push(currRow);
         }
         return gameBoard;
+      }
+
+      $scope.addPiece = function() {
+        console.log("Hello");
       }
 
       $scope.gameBoard=createGameBoard();
