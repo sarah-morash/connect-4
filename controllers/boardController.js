a42a371224a0ed212ce87e61ac3a87b5a7a68a76
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2016-11-15 13:47:06 -0400

    successfully injected service to check an inputted board for a winner into our main board controller

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,3 +1,4 @@
-angular.module("Connect4").controller('boardController', ['$scope', 'SquareBoard', function($scope, SquareBoard){
+angular.module("Connect4").controller('boardController', ['$scope', 'SquareBoard','CheckForWinner', function($scope, SquareBoard, CheckForWinner){
   $scope.gameBoard = SquareBoard.board(3);
+  $scope.isWinner = CheckForWinner.checkBoardForWinner($scope.gameBoard);
 }]);
