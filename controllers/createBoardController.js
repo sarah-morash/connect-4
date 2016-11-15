57dcf415706a612705de06882dc1fffd36337e2d
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2016-11-15 11:43:08 -0400

    more seperationg becoming apparent

--- controllers/createBoardController.js
+++ controllers/createBoardController.js
@@ -24,4 +24,80 @@ angular.module("Connect4").controller('createBoardController', function($scope){
     return gameBoard;
   }
   $scope.gameBoard = generateGameBoard();
+
+  /* This Function Shall Take As Input A Game
+  ** Board Object And Shall Check Each Row To
+  ** Determine If A Winning Row Combination
+  ** Exists, Returning The Corresponding Boolean
+  ** Result
+  */
+  function checkRowsForWinner(board){
+    for(var i = 0; i < board.rows.length; i++){
+      var winner = true;
+      for(var j = 0; j < board.rows[i].length; j++){
+        if (board.rows[i][j].piece === "_"){
+          winner=false;
+        }
+      }
+      if (winner){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /* This Function Shall Take As Input A Game
+  ** Board Object And Shall Check Each Column
+  ** To Determine If A Winning Column Combination
+  ** Exists, Returning The Corresponding Boolean
+  ** Value
+  */
+  function checkColumnsForWinner(board){
+    var columnHeight = board.rows[0].length;
+    for (var columnIndex = 0; columnIndex < columnHeight; columnIndex++){
+      winner = true;
+      for (var row = 0; row < board.rows.length; row ++){
+        if (board.rows[row][columnIndex].piece === "_"){
+          winner = false;
+        }
+      }
+      if (winner){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /* This Function Shall Take As Input A Game
+  ** Board Object And Shall Check Its Two Main
+  ** Diagonals For A Winning Combination
+  */
+  function checkDiagonalsForWinner(board){
+    var topLeftToBottomRightWinner = true;
+    var topRightToBottomLeftWinner = true;
+
+    for(var row = 0; row < board.rows.length; row++){
+      if (board.rows[row][row].piece === "_"){
+        topLeftToBottomRightWinner = false;
+      }
+    }
+
+    for (var row = 0; row < board.rows.length; row++){
+      if (board.rows[row][board.rows.length - 1 - row].piece === "_"){
+        topRightToBottomLeftWinner = false;
+      }
+    }
+  }
+
+  $scope.checkBoardForWinner = function(){
+    var isWinner = checkRowsForWinner($scope.gameBoard) || checkColumnsForWinner($scope.gameBoard) || checkDiagonalsForWinner($scope.gameBoard);
+
+    if (isWinner){
+      alert("We Have A Winner");
+    }
+
+    else{
+      alert("No Winner!");
+    }
+  }
 });
