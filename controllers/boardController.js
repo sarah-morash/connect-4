259c4adaed475166287fc114e8f0bceaaabd7f6f
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2016-11-15 15:43:07 -0400

    added basic boostrap styling

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -5,6 +5,16 @@ angular.module("Connect4").controller('boardController', ['$scope', 'SquareBoard
   $scope.addPiece = function(piece){
     AddPiece.addPieceToBoard(piece);
     $scope.isWinner = CheckForWinner.checkBoardForWinner($scope.gameBoard);
-  }
+  };
+
+  $scope.firstPlayer = {
+    name: 'Player 1',
+    gamePiece: 'X'
+  };
+
+  $scope.secondPlayer = {
+    name: 'Player2',
+    gamePiece: 'O'
+  };
 
 }]);
