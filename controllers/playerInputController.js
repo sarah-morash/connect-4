62cdc8c5b111efeea5459e967da57117a294fa1b
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2016-11-16 11:15:15 -0400

    basic first turn working

--- controllers/playerInputController.js
+++ controllers/playerInputController.js
@@ -5,16 +5,19 @@ angular.module('Connect4').controller('playerInputController', function($scope){
 
   $scope.player1 = {
     name: "",
-    piece: "X"
+    piece: "X",
+    isTurn: false
   };
 
   $scope.player2 = {
     name: "",
-    piece: "O"
+    piece: "O",
+    isTurn: false
   };
 
   $scope.setNames = function(){
     $scope.namesNeeded = false;
+    $scope.player1.isTurn = true;
   }
 
 });
